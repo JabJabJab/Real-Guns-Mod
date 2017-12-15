7751cb68d1590f8c7d86a69a3efcdb59a85070e0
FWolfe <FWolfe@users.noreply.github.com>
2017-12-15 10:12:11 -0800

    delayed to OnDistributionMerge event

--- ORGM/media/lua/server/ORGMCompatibility_ORGMSilencers_Server.lua
+++ ORGM/media/lua/server/ORGMCompatibility_ORGMSilencers_Server.lua
@@ -1,18 +1,18 @@
 --[[ Compatibility for Nolan's ORGMSilencer Mod
 
 ]]
-if ORGMUtil.isLoaded("ORGMSilencer") then
-    --getTexture("Item_Silencer.png")
-    
-    -- instead of adding the silencers as random loot, since ORGM Rechambered's Distribution file
-    -- isn't designed to handle non-ORGM items, we can add the silencers to the WeaponUpgrades
-    -- table instead. This will allow guns to spawn with silencers attached.
-    local silItem = InventoryItemFactory.CreateItem('Silencer.Silencer')
-    for gunName, gunData in pairs(ORGMMasterWeaponTable) do
-        local gunItem = getScriptManager():FindItem('ORGM.' .. gunName)
-        
-        if silItem:getMountOn():contains(gunItem:getDisplayName()) then
-            table.insert(WeaponUpgrades[gunName], 'Silencer.Silencer')
+Events.OnDistributionMerge.Add(function()
+    if ORGMUtil.isLoaded("ORGMSilencer") then
+        print("Adding silencers...")
+        -- instead of adding the silencers as random loot, since ORGM Rechambered's Distribution file
+        -- isn't designed to handle non-ORGM items, we can add the silencers to the WeaponUpgrades
+        -- table instead. This will allow guns to spawn with silencers attached.
+        local silItem = InventoryItemFactory.CreateItem('Silencer.Silencer')
+        for gunName, gunData in pairs(ORGMMasterWeaponTable) do
+            local gunItem = getScriptManager():FindItem('ORGM.' .. gunName)            
+            if silItem:getMountOn():contains(gunItem:getDisplayName()) then
+                table.insert(WeaponUpgrades[gunName], 'Silencer.Silencer')
+            end
         end
     end
-end 
+end)
