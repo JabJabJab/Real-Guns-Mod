ed375655f408495f898b33726362cfeda5d526c5
FWolfe <FWolfe@users.noreply.github.com>
2018-01-14 10:09:00 -0800

    Added Comments

--- examples/ORGM Military Only Automatics/media/lua/shared/ORGMMilOnlyAutos.lua
+++ examples/ORGM Military Only Automatics/media/lua/shared/ORGMMilOnlyAutos.lua
@@ -1,8 +1,11 @@
 
+-- All weapon defintions are contained within the ORGM.FirearmTable
 local FirearmTable = ORGM.FirearmTable
 
 for name, definition in pairs(FirearmTable) do
+    -- first find if the gun is a select fire automatic
     if definition.selectFire ~= nil then
+        -- change the rarity value. Valid options are nil, "Common", "Rare" and "VeryRare"
         definition.isCivilian = nil
         definition.isPolice = nil
         definition.isMilitary = "Rare"
