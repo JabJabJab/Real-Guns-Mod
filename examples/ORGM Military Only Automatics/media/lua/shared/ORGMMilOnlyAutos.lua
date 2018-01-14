9307e146e9552e193d79e0663dc667553055f495
FWolfe <FWolfe@users.noreply.github.com>
2018-01-14 10:12:39 -0800

    Updated Comments

--- examples/ORGM Military Only Automatics/media/lua/shared/ORGMMilOnlyAutos.lua
+++ examples/ORGM Military Only Automatics/media/lua/shared/ORGMMilOnlyAutos.lua
@@ -6,8 +6,12 @@ for name, definition in pairs(FirearmTable) do
     -- first find if the gun is a select fire automatic
     if definition.selectFire ~= nil then
         -- change the rarity value. Valid options are nil, "Common", "Rare" and "VeryRare"
+        -- case matters: "rare" is not a valid value.
         definition.isCivilian = nil
         definition.isPolice = nil
         definition.isMilitary = "Rare"
     end
 end
+
+-- note if changing values after the game has started (ie: on events) it is necessary to call
+-- ORGM.Server.buildRarityTables()
