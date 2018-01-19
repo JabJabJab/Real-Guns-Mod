8ed4f3227c9012b859bb3216e27bfec7d1014c16
FWolfe <FWolfe@users.noreply.github.com>
2018-01-19 12:05:56 -0800

    updated backwards compatibility functions

--- ORGM/media/lua/client/1LoadOrder/ORGMClientFunctions.lua
+++ ORGM/media/lua/client/1LoadOrder/ORGMClientFunctions.lua
@@ -1,41 +1,41 @@
 ORGM.Client.loadModels = function()
-	local dir = getDir("ORGM")
-	local locationModel = dir .. "/media/models/weapons_"
-    local locationTexture = dir .. "/media/textures/Objects_"
+    local dir = getDir("ORGM")
+    local modelPrefix = dir .. "/media/models/weapons_"
+    local texturePrefix = dir .. "/media/textures/Objects_"
     
-	--                    (model name        , modelLocation, textureLocation )
-	loadStaticZomboidModel("weapons_ar15", locationModel .. "ar15.txt", locationTexture .. "ar15.png")
-	loadStaticZomboidModel("weapons_fnfal", locationModel .. "fnfal.txt", locationTexture .. "fnfal.png")
-	loadStaticZomboidModel("weapons_g3", locationModel .. "g3.txt", locationTexture .. "g3.png")
-	loadStaticZomboidModel("weapons_kalash", locationModel .. "kalash.txt", locationTexture .. "kalash.png")
-	loadStaticZomboidModel("weapons_kriss", locationModel .. "kriss.txt", locationTexture .. "kriss.png")
-	loadStaticZomboidModel("weapons_krissciv", locationModel .. "krissciv.txt", locationTexture .. "krissciv.png")
-	loadStaticZomboidModel("weapons_m16", locationModel .. "m16.txt", locationTexture .. "m16.png")
-	loadStaticZomboidModel("weapons_mp5", locationModel .. "mp5.txt", locationTexture .. "mp5.png")
-	loadStaticZomboidModel("weapons_revolverlarge", locationModel .. "revolverlarge.txt", locationTexture .. "revolverlarge.png")
-	loadStaticZomboidModel("weapons_ump", locationModel .. "ump.txt", locationTexture .. "ump.png")
-	loadStaticZomboidModel("weapons_uzi", locationModel .. "uzi.txt", locationTexture .. "uzi.png")
-	loadStaticZomboidModel("weapons_shotgunsawn", locationModel .. "shotgunsawn.txt", locationTexture .. "ShotgunSawn.png") 
-	loadStaticZomboidModel("weapons_shotgunsawnblack", locationModel .. "shotgunsawnblack.txt", locationTexture .. "ShotgunSawn_Black.png") 
-	loadStaticZomboidModel("weapons_shotgun", locationModel .. "shotgun.txt", locationTexture .. "Shotgun.png") 
-	loadStaticZomboidModel("weapons_shotgunblack", locationModel .. "shotgunblack.txt", locationTexture .. "Shotgun_Black.png") 
-	loadStaticZomboidModel("weapons_spas12", locationModel .. "spas12.txt", locationTexture .. "spas12.png") 
-	loadStaticZomboidModel("weapons_glock22", locationModel .. "glock22.txt", locationTexture .. "glock22.png") 
-	loadStaticZomboidModel("weapons_glock23", locationModel .. "glock23.txt", locationTexture .. "glock23.png") 
-	loadStaticZomboidModel("weapons_model19bwg", locationModel .. "model19bwg.txt", locationTexture .. "model19_Black_WoodGrip.png") 
-	loadStaticZomboidModel("weapons_model19cwg", locationModel .. "model19cwg.txt", locationTexture .. "model19_Chrome_WoodGrip.png") 
-	loadStaticZomboidModel("weapons_model19cbg", locationModel .. "model19cbg.txt", locationTexture .. "model19_Chrome_BlackGrip.png") 
-	loadStaticZomboidModel("weapons_rugermkii", locationModel .. "rugermkii.txt", locationTexture .. "rugermkii.png") 
-	loadStaticZomboidModel("weapons_henry", locationModel .. "henry.txt", locationTexture .. "henry.png") 
-	loadStaticZomboidModel("weapons_m14", locationModel .. "m14.txt", locationTexture .. "m14.png") 
-	loadStaticZomboidModel("weapons_p90", locationModel .. "p90.txt", locationTexture .. "p90.png") 
-	loadStaticZomboidModel("weapons_sa80", locationModel .. "sa80.txt", locationTexture .. "sa80.png") 
-	loadStaticZomboidModel("weapons_sks", locationModel .. "sks.txt", locationTexture .. "sks.png") 
-	loadStaticZomboidModel("weapons_svd", locationModel .. "svd.txt", locationTexture .. "svd.png") 
-	loadStaticZomboidModel("weapons_mini14", locationModel .. "mini14.txt", locationTexture .. "mini14.png") 
-	
+    --                    (model name        , modelLocation, textureLocation )
+    loadStaticZomboidModel("weapons_ar15", modelPrefix .. "ar15.txt", texturePrefix .. "ar15.png")
+    loadStaticZomboidModel("weapons_fnfal", modelPrefix .. "fnfal.txt", texturePrefix .. "fnfal.png")
+    loadStaticZomboidModel("weapons_g3", modelPrefix .. "g3.txt", texturePrefix .. "g3.png")
+    loadStaticZomboidModel("weapons_kalash", modelPrefix .. "kalash.txt", texturePrefix .. "kalash.png")
+    loadStaticZomboidModel("weapons_kriss", modelPrefix .. "kriss.txt", texturePrefix .. "kriss.png")
+    loadStaticZomboidModel("weapons_krissciv", modelPrefix .. "krissciv.txt", texturePrefix .. "krissciv.png")
+    loadStaticZomboidModel("weapons_m16", modelPrefix .. "m16.txt", texturePrefix .. "m16.png")
+    loadStaticZomboidModel("weapons_mp5", modelPrefix .. "mp5.txt", texturePrefix .. "mp5.png")
+    loadStaticZomboidModel("weapons_revolverlarge", modelPrefix .. "revolverlarge.txt", texturePrefix .. "revolverlarge.png")
+    loadStaticZomboidModel("weapons_ump", modelPrefix .. "ump.txt", texturePrefix .. "ump.png")
+    loadStaticZomboidModel("weapons_uzi", modelPrefix .. "uzi.txt", texturePrefix .. "uzi.png")
+    loadStaticZomboidModel("weapons_shotgunsawn", modelPrefix .. "shotgunsawn.txt", texturePrefix .. "ShotgunSawn.png") 
+    loadStaticZomboidModel("weapons_shotgunsawnblack", modelPrefix .. "shotgunsawnblack.txt", texturePrefix .. "ShotgunSawn_Black.png") 
+    loadStaticZomboidModel("weapons_shotgun", modelPrefix .. "shotgun.txt", texturePrefix .. "Shotgun.png") 
+    loadStaticZomboidModel("weapons_shotgunblack", modelPrefix .. "shotgunblack.txt", texturePrefix .. "Shotgun_Black.png") 
+    loadStaticZomboidModel("weapons_spas12", modelPrefix .. "spas12.txt", texturePrefix .. "spas12.png") 
+    loadStaticZomboidModel("weapons_glock22", modelPrefix .. "glock22.txt", texturePrefix .. "glock22.png") 
+    loadStaticZomboidModel("weapons_glock23", modelPrefix .. "glock23.txt", texturePrefix .. "glock23.png") 
+    loadStaticZomboidModel("weapons_model19bwg", modelPrefix .. "model19bwg.txt", texturePrefix .. "model19_Black_WoodGrip.png") 
+    loadStaticZomboidModel("weapons_model19cwg", modelPrefix .. "model19cwg.txt", texturePrefix .. "model19_Chrome_WoodGrip.png") 
+    loadStaticZomboidModel("weapons_model19cbg", modelPrefix .. "model19cbg.txt", texturePrefix .. "model19_Chrome_BlackGrip.png") 
+    loadStaticZomboidModel("weapons_rugermkii", modelPrefix .. "rugermkii.txt", texturePrefix .. "rugermkii.png") 
+    loadStaticZomboidModel("weapons_henry", modelPrefix .. "henry.txt", texturePrefix .. "henry.png") 
+    loadStaticZomboidModel("weapons_m14", modelPrefix .. "m14.txt", texturePrefix .. "m14.png") 
+    loadStaticZomboidModel("weapons_p90", modelPrefix .. "p90.txt", texturePrefix .. "p90.png") 
+    loadStaticZomboidModel("weapons_sa80", modelPrefix .. "sa80.txt", texturePrefix .. "sa80.png") 
+    loadStaticZomboidModel("weapons_sks", modelPrefix .. "sks.txt", texturePrefix .. "sks.png") 
+    loadStaticZomboidModel("weapons_svd", modelPrefix .. "svd.txt", texturePrefix .. "svd.png") 
+    loadStaticZomboidModel("weapons_mini14", modelPrefix .. "mini14.txt", texturePrefix .. "mini14.png") 
+    
     ORGM.log(ORGM.INFO, "All 3d models loaded.")
-end	
+end 
 
 --[[  ORGM.Client.checkFirearmBuildID(player, item)
     
@@ -46,7 +46,25 @@ end
 ]]
 ORGM.Client.checkFirearmBuildID = function(player, item)
     if item == nil or player == nil then return end
-    if ORGM.checkFirearmBuildID(item, player:getInventory()) then
-        player:Say("Resetting this weapon to defaults due to ORGM changes. Unequipping.")
+    ORGM.log(ORGM.DEBUG, "Checking BUILD_ID for ".. item:getType())
+    if ORGM.checkFirearmBuildID(item) then
+        player:Say("Resetting this weapon to defaults due to ORGM changes. Ammo returned to inventory.")
+        ORGM.Client.unequipItemNow(player, item)
+        local newItem = ORGM.replaceFirearmWithNewCopy(item, player:getInventory())
+        player:setPrimaryHandItem(newItem)
+        ISInventoryPage.dirtyUI()
     end
+end
+
+ORGM.Client.unequipItemNow = function(player, item)
+    item:getContainer():setDrawDirty(true)
+    local primary = player:getPrimaryHandItem()
+    local secondary = player:getSecondaryHandItem()
+    if item == primary then
+        if (item:isTwoHandWeapon() or item:isRequiresEquippedBothHands()) and item == secondary then
+            player:setSecondaryHandItem(nil)
+        end
+        player:setPrimaryHandItem(nil)
+    end
+    getPlayerData(player:getPlayerNum()).playerInventory:refreshBackpacks()
 end
