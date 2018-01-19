1c5a486fc6bebb3098d40a1101a296370e5f01f9
FWolfe <FWolfe@users.noreply.github.com>
2018-01-18 22:24:13 -0800

    added backwards compatibility event

--- ORGM/media/lua/shared/ORGMSharedEventHooks.lua
+++ ORGM/media/lua/shared/ORGMSharedEventHooks.lua
@@ -1,6 +1,8 @@
 Events.OnGameBoot.Add(ORGM.validateSettings)
 Events.OnGameBoot.Add(ORGM.limitFirearmYear)
+Events.OnGameBoot.Add(ORGM.onBootBackwardsCompatibility) -- To be removed at a later date.
 Events.OnGameBoot.Add(ORGM.loadCompatibilityPatches)
+
 Events.OnLoadSoundBanks.Add(ORGM.onLoadSoundBanks)
 
 -- TODO: move this one
