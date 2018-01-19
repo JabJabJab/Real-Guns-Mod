f90be14c9e393c53e9b02dc7b20e2b082c9572d2
FWolfe <FWolfe@users.noreply.github.com>
2018-01-19 12:09:33 -0800

    removed redundant table ORGM.FIREARM_HISTORY

--- ORGM/media/lua/shared/ORGMBackwardsCompatibility.lua
+++ ORGM/media/lua/shared/ORGMBackwardsCompatibility.lua
@@ -1,9 +1,11 @@
 --[[
-    Everything in here exists for backwards compatibility
+    Everything in here exists for backwards compatibility with ORGM 2.00 to 2.03
+    This file will be removed at a later date.
+    If you have a 3d party mod that uses any of these tables it needs to be updated.
 ]]
 
 
--- these tables and function will be removed at a later time.
+
 ORGMAlternateAmmoTable = ORGM.AlternateAmmoTable
 ORGMMasterAmmoTable = ORGM.AmmoTable
 ORGMMasterMagTable = ORGM.MagazineTable
@@ -19,26 +21,3 @@ ORGM.onBootBackwardsCompatibility = function()
         table.insert(ORGMRepairKitsTable, key)
     end
 end
-
-
---[[    This is the backwards compatibility internal changelog for firearms
-    If the guns ModData.BUILD_ID is lower then the id's listed below, the weapon
-    needs to be reset to default values. It is only necessary to list weapons
-    here when the stats have changed on ORGM updates.
-
-]]
-ORGM.FIREARM_HISTORY['CZ75'] = 12 -- changed in 3.00-alpha
-ORGM.FIREARM_HISTORY['Taurus38'] = 12 -- changed in 3.00-alpha
-ORGM.FIREARM_HISTORY['BLR'] = 12 -- changed in 3.00-alpha
-ORGM.FIREARM_HISTORY['HenryBB'] = 12 -- changed in 3.00-alpha
-ORGM.FIREARM_HISTORY['M1903'] = 12 -- changed in 3.00-alpha
-
-
-
-
-
-
-
-
-
-
