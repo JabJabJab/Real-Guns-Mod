cc63717057435ae9c666d3649a4908f37b162d6d
FWolfe <FWolfe@users.noreply.github.com>
2018-01-18 22:23:51 -0800

    added internal changelog for firearms

--- ORGM/media/lua/shared/ORGMBackwardsCompatibility.lua
+++ ORGM/media/lua/shared/ORGMBackwardsCompatibility.lua
@@ -1,42 +1,44 @@
 --[[
-    Everything in here exists for backwards compatibility with Rechambered 2.03
+    Everything in here exists for backwards compatibility
 ]]
 
 
-
+-- these tables and function will be removed at a later time.
 ORGMAlternateAmmoTable = ORGM.AlternateAmmoTable
 ORGMMasterAmmoTable = ORGM.AmmoTable
 ORGMMasterMagTable = ORGM.MagazineTable
 ORGMMasterWeaponTable = ORGM.FirearmTable
+ORGMRepairKitsTable = { }
+ORGMWeaponModsTable = { }
 
---[[ ORGMWeaponModsTable (Global)
-    A list of weapon repair kits in ORGM, this is used for distribution tables, and so other mods can
-    list possible upgrades without having to hardcode the list
+ORGM.onBootBackwardsCompatibility = function()
+    for key, value in pairs(ORGM.ComponentTable) do
+        table.insert(ORGMWeaponModsTable, key)
+    end
+    for key, value in pairs(ORGM.RepairKitTable) do
+        table.insert(ORGMRepairKitsTable, key)
+    end
+end
+
+
+--[[    This is the backwards compatibility internal changelog for firearms
+    If the guns ModData.BUILD_ID is lower then the id's listed below, the weapon
+    needs to be reset to default values. It is only necessary to list weapons
+    here when the stats have changed on ORGM updates.
+
 ]]
-ORGMRepairKitsTable = { "WD40", "Brushkit", "Maintkit" }
+ORGM.FIREARM_HISTORY['CZ75'] = 12 -- changed in 3.00-alpha
+ORGM.FIREARM_HISTORY['Taurus38'] = 12 -- changed in 3.00-alpha
+ORGM.FIREARM_HISTORY['BLR'] = 12 -- changed in 3.00-alpha
+ORGM.FIREARM_HISTORY['HenryBB'] = 12 -- changed in 3.00-alpha
+ORGM.FIREARM_HISTORY['M1903'] = 12 -- changed in 3.00-alpha
 
 
---[[ ORGMWeaponModsTable (Global)
-    A list of standard weapon mods in ORGM, this is used for distribution tables, and so other mods can
-    list possible upgrades without having to hardcode the list
-]]
-ORGMWeaponModsTable = {
-    '2xScope', 
-    '4xScope', 
-    '8xScope', 
-    'FibSig', 
-    'Foregrip', 
-    'FullCh', 
-    'HalfCh', 
-    'PistolLas', 
-    'PistolTL', 
-    'RDS', 
-    'Recoil', 
-    'Reflex', 
-    'RifleLas', 
-    'RifleTL', 
-    'Rifsling', 
-    'SkeletalStock', 
-    'CollapsingStock'
-}
+
+
+
+
+
+
+
 
