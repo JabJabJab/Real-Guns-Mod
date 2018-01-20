ae4e7de51fcd81ed84b2d36106933a49d8ef91c6
FWolfe <FWolfe@users.noreply.github.com>
2018-01-20 10:12:27 -0800

    TODO comment

--- ORGM/media/lua/client/ORGMClientEventHooks.lua
+++ ORGM/media/lua/client/ORGMClientEventHooks.lua
@@ -6,6 +6,11 @@ Events.OnFillInventoryObjectContextMenu.Add(ORGM.Client.inventoryContextMenu)
 Events.OnGameBoot.Add(ORGM.Client.loadModels)
 Events.OnGameBoot.Add(ORGM.Client.loadCompatibilityPatches)
 Events.OnEquipPrimary.Add(ORGM.Client.checkFirearmBuildID)
+
+-- TODO: this should really not be written like this, as it doesn't allow
+-- for third party mods to overwrite or remove the event. Not sure why
+-- they'd want to since this is a pretty important backwards compatibility
+-- check..
 Events.OnGameStart.Add(function() 
     local player = getPlayer()
     local item = player:getPrimaryHandItem()
