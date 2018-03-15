c0769ae8f6f01440b2412946f25c9d3406412185
FWolfe <FWolfe@users.noreply.github.com>
2018-03-15 11:11:43 -0700

    removed tactical light hotkey event (redundant)

--- ORGM/media/lua/shared/ORGMSharedEventHooks.lua
+++ ORGM/media/lua/shared/ORGMSharedEventHooks.lua
@@ -7,9 +7,11 @@ Events.OnGameBoot.Add(ORGM.loadCompatibilityPatches)
 Events.OnLoadSoundBanks.Add(ORGM.onLoadSoundBanks)
 
 -- TODO: move this one, server function?
+--[[
 Events.OnKeyPressed.Add(function(key)
     local player = getSpecificPlayer(0)
     if key == getCore():getKey("Equip/Turn On/Off Light Source") then
         ORGM.toggleTacticalLight(player)
     end
 end)
+]]
