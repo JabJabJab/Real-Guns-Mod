4b026b091a084ab203fb526bf1ea2674bfd65c5b
FWolfe <FWolfe@users.noreply.github.com>
2018-03-15 11:38:36 -0700

    new category constants, updated build history

--- ORGM/media/lua/shared/1LoadOrder/ORGMCore.lua
+++ ORGM/media/lua/shared/1LoadOrder/ORGMCore.lua
@@ -16,7 +16,7 @@ ORGM = {
     BUILD_HISTORY = {
         "2.00-alpha", "2.00-beta-rc1", "2.00-beta-rc2", "2.00-beta-rc3", "2.00-beta-rc4", "2.00-beta-rc5", "2.00-beta-rc6", -- 7
         "2.00-stable", "2.01-stable", "2.02-stable", "2.03-stable", -- 11
-        "3.00-alpha", "3.00-beta-rc1", "3.00-beta-rc2", "3.00-stable", "3.01-stable", "3.02-stable",-- 17
+        "3.00-alpha", "3.00-beta-rc1", "3.00-beta-rc2", "3.00-stable", "3.01-stable", "3.02-stable", "3.03-stable",-- 18
     },
     BUILD_ID = nil, -- set automatically at the end of this file
 
@@ -39,6 +39,13 @@ ORGM = {
     BREAK = 5,
     ROTARY = 6,
 
+    -- category constants
+    PISTOL = 1,
+    REVOLVER = 2,
+    SUBMACHINEGUN = 3,
+    RIFLE = 4,
+    SHOTGUN = 5,
+    
     -- select fire mode constants
     SEMIAUTOMODE = 0,
     FULLAUTOMODE = 1,
