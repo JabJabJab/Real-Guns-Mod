f411768940369c37d03dd1bd7956abfdcd244057
FWolfe <FWolfe@users.noreply.github.com>
2018-03-18 15:17:20 -0700

    Removed commented out stuff

--- ORGM/media/lua/shared/ORGMSharedEventHooks.lua
+++ ORGM/media/lua/shared/ORGMSharedEventHooks.lua
@@ -6,12 +6,3 @@ Events.OnGameBoot.Add(ORGM.loadCompatibilityPatches)
 
 Events.OnLoadSoundBanks.Add(ORGM.onLoadSoundBanks)
 
--- TODO: move this one, server function?
---[[
-Events.OnKeyPressed.Add(function(key)
-    local player = getSpecificPlayer(0)
-    if key == getCore():getKey("Equip/Turn On/Off Light Source") then
-        ORGM.toggleTacticalLight(player)
-    end
-end)
-]]
