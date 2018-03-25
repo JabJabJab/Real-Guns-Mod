033eb09f90e6369edecf2db7c593905ec35120b1
FWolfe <FWolfe@users.noreply.github.com>
2018-03-25 08:47:39 -0700

    added debugging window to Inspection

--- ORGM/media/lua/client/ORGMInspectionWindow.lua
+++ ORGM/media/lua/client/ORGMInspectionWindow.lua
@@ -8,20 +8,40 @@ local InfoPanel = ISPanelJoypad:derive("InfoPanel")
 
 function InfoPanel:new(x, y, width, height)
     local o = ISPanelJoypad:new(x, y, width, height)
-    o:noBackground()
     setmetatable(o, self)
-    self.__index = self
+    o:noBackground()
+    o.__index = self
+    o.resizable = false
     return o
 end
 
 function InfoPanel:createChildren()
-    self.textPanel = ISRichTextPanel:new(8, 16, self.width-8, self.height-8)
+    self.textPanel = ISRichTextPanel:new(8, 16, self.width-16, self.height-24)
     self.textPanel:initialise()
     self.textPanel.autosetheight = false
     self.textPanel:ignoreHeightChange()
     self:addChild(self.textPanel)
 end
 
+function InfoPanel:updateFirearm(item)
+    if not item then return end
+    local def = ORGM.getFirearmData(item)
+    if not def then 
+        self.textPanel.text = ""
+        self.textPanel:paginate()
+        return
+    end
+    local data = item:getModData()
+    
+    local text = " <RED> <CENTER> ".. item:getDisplayName() .. " <LINE> <LEFT> <TEXT> <LINE> "
+    text = text .. getText("IGUI_Firearm_InfoPanel", getText(def.classification), (def.year or getText("IGUI_Firearm_YearUnknown")), getText(def.country), getText(def.manufacturer))
+
+    text = text .. getText("IGUI_Firearm_InfoBackGround")
+    text = text .. getText(def.description) .. " <LINE> <LINE> "
+    self.textPanel.text = text
+    self.textPanel:paginate()
+
+end
 
 -------------------------------------------------------------------------------------------
 local DetailsPanel = ISPanelJoypad:derive("DetailsPanel")
@@ -29,62 +49,33 @@ local DetailsPanel = ISPanelJoypad:derive("DetailsPanel")
 function DetailsPanel:new(x, y, width, height)
     local o = ISPanelJoypad:new(x, y, width, height)
     setmetatable(o, self)
-    self:noBackground()
-    self.__index = self
-    self.resizable = false
+    o:noBackground()
+    o.__index = self
+    o.resizable = false
     return o
 end
 
 function DetailsPanel:createChildren()
-    self.textPanel = ISRichTextPanel:new(8, 16, self.width-8, self.height-8)
+    self.textPanel = ISRichTextPanel:new(8, 16, self.width-16, self.height-24)
     self.textPanel:initialise()
     self.textPanel.autosetheight = false
     self.textPanel:ignoreHeightChange()
     self:addChild(self.textPanel)
 end
 
--------------------------------------------------------------------------------------------
-
-ORGMFirearmWindow = ISCollapsableWindow:derive("ORGMFirearmWindow")
-ORGMFirearmWindow.view = {}
-
-
-function ORGMFirearmWindow:initialise()
-    ISCollapsableWindow.initialise(self)
-end
-
-function ORGMFirearmWindow:new(x, y, width, height)
-    local o = ISCollapsableWindow:new(x, y, width, height);
-    setmetatable(o, self)
-    self.__index = self;
-    o.title = getText("IGUI_Firearm_InspectTitle")
-    o.pin = false
-    o:noBackground()
-    self.resizable = false
-    return o
-end
-
-
-
-function ORGMFirearmWindow:setFirearm(item)
-    local itemType = item:getType()
-    local def = ORGM.getFirearmData(itemType)
-    local scriptItem = item:getScriptItem() -- getScriptManager():FindItem(def.moduleName ..'.' .. itemType)
-    local data = item:getModData()
+function DetailsPanel:updateFirearm(item)
+    local def = ORGM.getFirearmData(item)
+    if not def then 
+        self.textPanel.text = ""
+        self.textPanel:paginate()
+        return
+    end
+    local data = item:getModData()    
+    local text = " <RED> <CENTER> ".. item:getDisplayName() .. " <LINE> <LEFT> <TEXT> <LINE> "
     
-    
-    local text = scriptItem:getDisplayName() .. "\n"
-    text = text .. getText("IGUI_Firearm_InfoPanel", getText(def.classification), (def.year or getText("IGUI_Firearm_YearUnknown")), getText(def.country), getText(def.manufacturer))
-
-    text = text .. getText(def.description)
-    self.infoPanel.textPanel.text = text
-    self.infoPanel.textPanel:paginate()
-    
-    text = scriptItem:getDisplayName() .. "\n"
-    
-    if data.selectFire ~= 0 then
+    if data.selectFire then
         local mode = "IGUI_Firearm_DetailSemi"
-        if data.selectFire == 1 then mode = "IGUI_Firearm_DetailFull" end
+        if data.selectFire == ORGM.FULLAUTOMODE then mode = "IGUI_Firearm_DetailFull" end
         text = text .. getText("IGUI_Firearm_DetailMode", getText(mode))
     end
     
@@ -185,29 +176,169 @@ function ORGMFirearmWindow:setFirearm(item)
         text = text.. getText("IGUI_Firearm_DetailSerial", data.serialnumber)
     end
     
-    self.detailsPanel.textPanel.text = text
-    self.detailsPanel.textPanel:paginate()
+    self.textPanel.text = text
+    self.textPanel:paginate()
 
 end
 
+local DebugPanel = ISPanelJoypad:derive("DebugPanel")
+
+function DebugPanel:new(x, y, width, height)
+    local o = ISPanelJoypad:new(x, y, width, height)
+    setmetatable(o, self)
+    o:noBackground()
+    o.__index = self
+    o.resizable = false
+    return o
+end
+
+function DebugPanel:createChildren()
+    self.textPanel = ISRichTextPanel:new(8, 16, self.width-16, self.height-24)
+    self.textPanel:initialise()
+    self.textPanel.autosetheight = false
+    self.textPanel:ignoreHeightChange()
+    self:addChild(self.textPanel)
+end
+
+function DebugPanel:updateFirearm(item)
+    local def = ORGM.getFirearmData(item)
+    if not def then --or not ORGM.Settings.Debug then 
+        self.textPanel.text = ""
+        self.textPanel:paginate()
+        return
+    end
+--    local scriptItem = item:getScriptItem() -- getScriptManager():FindItem(def.moduleName ..'.' .. itemType)
+    local data = item:getModData()
+    
+    local text = " <RED> <CENTER> ".. item:getDisplayName() .. " <LINE> <LEFT> <TEXT> <LINE> "
+    local player = getPlayer()
+    text = text .. getText("IGUI_Firearm_DebugState1", data.actionType, data.triggerType)
+    if data.selectFire then
+        local mode = "IGUI_Firearm_DetailSemi"
+        if data.selectFire == ORGM.FULLAUTOMODE then mode = "IGUI_Firearm_DetailFull" end
+        text = text .. getText("IGUI_Firearm_DebugMode", getText(mode))
+    end
+    
+    local player = getSpecificPlayer(0)
+    local beenMoving = player:getBeenMovingFor()
+    local aimingPerk = player:getPerkLevel(Perks.Aiming)
+    local hitChanceMod = item:getHitChance()
+    -- translated from the java
+    hitChanceMod = hitChanceMod + item:getAimingPerkHitChanceModifier() * aimingPerk
+    local hitChancePenalty = 0
+    if player:IsAiming() and beenMoving > item:getAimingTime() + aimingPerk * 2 then
+        hitChancePenalty = (beenMoving - (item:getAimingTime() + aimingPerk * 2)) * -1
+    end
+    hitChanceMod = hitChanceMod + hitChancePenalty
+    
+    if player:HasTrait("Marksman") then hitChanceMod = hitChanceMod + 20 end
+    --if hitChanceMod < 10 then hitChanceMod = 10 end
+    --if hitChanceMod > 100 then hitChanceMod = 100 end
+    --[[
+    local damageMax = item:getMaxDamage()
+    local damageMin = item:getMinDamage()
+
+    --dam /= armsPain / 10.0F;
+    if player:HasTrait("Underweight") then
+        damageMin = damageMin * 0.8
+        damageMax = damageMax * 0.8
+    elseif player.HasTrait("Very Underweight") then
+        damageMin = damageMin * 0.6
+        damageMax = damageMax * 0.6
+    elseif player.HasTrait("Emaciated")) then
+        damageMin = damageMin * 0.4
+        damageMax = damageMax * 0.4
+    end
+    
+    damageMin = damageMin / (1 / 2.0F)
+    damageMax = damageMax / (1 / 2.0F)
+    local endurance = player:getMoodles():getMoodleLevel(MoodleType.Endurance)
+    if endurance == 1 then
+        damageMin = damageMin * 0.5
+        damageMax = damageMax * 0.5
+    elseif endurance == 2 then
+        damageMin = damageMin * 0.2
+        damageMax = damageMax * 0.2
+    elseif endurance == 4 then
+        damageMin = damageMin * 0.1
+        damageMax = damageMax * 0.1
+    elseif endurance == 4 then
+        damageMin = damageMin * 0.05
+        damageMax = damageMax * 0.05
+    end
+    ]]
+    
+    text = text .. getText("IGUI_Firearm_DebugState2", tostring(data.hammerCocked == 1 or false), tostring(data.isOpen == 1 or false))
+    text = text .. getText("IGUI_Firearm_DebugWeight", tostring(item:getWeight()), tostring(item:getActualWeight()))
+    text = text .. getText("IGUI_Firearm_DebugCapacity", tostring(data.currentCapacity), tostring(data.maxCapacity), tostring(data.roundChambered), tostring(data.emptyShellChambered))
+    text = text .. getText("IGUI_Firearm_DebugAmmo", tostring(data.lastRound), tostring(data.preferredAmmoType), tostring(data.loadedAmmo), tostring(data.isJammed))
+    text = text .. getText("IGUI_Firearm_DebugAccuracy", tostring(item:getHitChance()), tostring(hitChanceMod), tostring(item:getAimingPerkHitChanceModifier() * aimingPerk), tostring(hitChancePenalty))
+    text = text .. getText("IGUI_Firearm_DebugAccuracy2", tostring(item:getCriticalChance()))
+    text = text .. getText("IGUI_Firearm_DebugRange", tostring(item:getMinRange()), tostring(item:getMaxRange()))
+    text = text .. getText("IGUI_Firearm_DebugDamage", tostring(item:getMinDamage()), tostring(item:getMaxDamage()), tostring(item:getTreeDamage()), tostring(item:getDoorDamage()))
+    text = text .. getText("IGUI_Firearm_DebugSpeed", tostring(item:getSwingTime()), tostring(item:getRecoilDelay()))
+    text = text .. getText("IGUI_Firearm_DebugSpeed2", tostring(item:getAimingTime()), tostring(beenMoving))
+    text = text .. getText("IGUI_Firearm_DebugMagazine")
+    text = text .. getText("IGUI_Firearm_DebugCylinder", tostring(data.cylinderPosition))
+    for i=1, data.currentCapacity do
+        text = text .. "#" .. i .. ": "..tostring(data.magazineData[i]) .. " <LINE> "
+    end
+    --getDamageMod(IsoGameCharacter chr) 
+    self.textPanel.text = text
+    self.textPanel:paginate()
+end
+-------------------------------------------------------------------------------------------
+
+ORGMFirearmWindow = ISCollapsableWindow:derive("ORGMFirearmWindow")
+ORGMFirearmWindow.view = {}
+
+
+function ORGMFirearmWindow:initialise()
+    ISCollapsableWindow.initialise(self)
+end
+
+function ORGMFirearmWindow:new(x, y, width, height)
+    local o = ISCollapsableWindow:new(x, y, width, height);
+    setmetatable(o, self)
+    self.__index = self;
+    o.title = getText("IGUI_Firearm_InspectTitle")
+    o.pin = false
+    o:noBackground()
+    self.resizable = false
+    return o
+end
 
 function ORGMFirearmWindow:createChildren()
     ISCollapsableWindow.createChildren(self)
     local th = self:titleBarHeight()
-    local rh = self:resizeWidgetHeight()
-    self.panel = ISTabPanel:new(0, th, self.width, self.height-th-rh)
+    --local rh = self:resizeWidgetHeight()
+    --self.panel = ISTabPanel:new(0, th, self.width, self.height-th-rh)
+    self.panel = ISTabPanel:new(0, th, self.width, self.height-th)
     self.panel:initialise()
     self.panel.allowDraggingTab = false
     self:addChild(self.panel)
 
-    self.infoPanel = InfoPanel:new(0, 8, self.width, self.height-8)
+    self.infoPanel = InfoPanel:new(0, 8, self.width, self.height-16)
     self.infoPanel:initialise()
     self.panel:addView(getText("IGUI_Firearm_InfoTitle"), self.infoPanel)
     
-    self.detailsPanel = DetailsPanel:new(0, 8, self.width, self.height-8)
+    self.detailsPanel = DetailsPanel:new(0, 8, self.width, self.height-16)
     self.detailsPanel:initialise()
     self.panel:addView(getText("IGUI_Firearm_DetailTitle"), self.detailsPanel)
     
+    --if ORGM.Settings.Debug then
+        self.debugPanel = DebugPanel:new(0, 8, self.width, self.height-16)
+        self.debugPanel:initialise()
+        self.panel:addView(getText("IGUI_Firearm_DebugTitle"), self.debugPanel)
+    --end
+end
+
+
+
+function ORGMFirearmWindow:setFirearm(item)
+    if not self:isVisible() then return end
+    local view = self.panel:getActiveView()
+    if view then view:updateFirearm(item) end
 end
 
 function ORGMFirearmWindow:isActive(viewName)
