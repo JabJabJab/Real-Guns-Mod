2a092865165bfac917eadfae8ada3bc8b84d6bb7
FWolfe <FWolfe@users.noreply.github.com>
2018-03-29 18:17:16 -0700

    Changed reload mag hotkey to G

--- ORGM/media/lua/client/1LoadOrder/ORGMClientOptions.lua
+++ ORGM/media/lua/client/1LoadOrder/ORGMClientOptions.lua
@@ -12,7 +12,7 @@ if index then
     table.insert(keyBinding, index+1, {value = "Equip/Unequip Pistol", key = 5})
     table.insert(keyBinding, index+2, {value = "Equip/Unequip Rifle", key = 6})
     table.insert(keyBinding, index+3, {value = "Equip/Unequip Shotgun", key = 7})
-    table.insert(keyBinding, index+4, {value = "Reload Any Magazine", key = Keyboard.KEY_Y })
+    table.insert(keyBinding, index+4, {value = "Reload Any Magazine", key = Keyboard.KEY_G })
     table.insert(keyBinding, index+5, {value = "Select Fire Toggle", key = Keyboard.KEY_Z })
     table.insert(keyBinding, index+6, {value = "Firearm Inspection Window", key = Keyboard.KEY_U })
     
