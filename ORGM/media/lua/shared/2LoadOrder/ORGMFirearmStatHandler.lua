e78d42afe524da38bfd8e6662a3cf950449ac2f6
FWolfe <noreply@tekagis.ca>
2018-07-12 15:02:07 -0700

    adjust recoil, range, damage based on barrel len

--- ORGM/media/lua/shared/2LoadOrder/ORGMFirearmStatHandler.lua
+++ ORGM/media/lua/shared/2LoadOrder/ORGMFirearmStatHandler.lua
@@ -4,12 +4,12 @@ local Settings = ORGM.Settings
 local ABS_FULLAUTOSWINGTIME = 0.3 -- full auto only, anything else is dynamic
 --local ABS_CRITICALCHANCE = 20 -- default
 --local ABS_AIMINGPERKCRITMOD = 10 -- default
-local ABS_AIMINGPERKHITMOD = 7 -- default
+--local ABS_AIMINGPERKHITMOD = 7 -- default
 
 -- Adjustment Constants
 local ADJ_FULLAUTOHITCHANCE = -10  -- full auto only
 local ADJ_FULLAUTORECOILDELAY = -20 -- full auto only
-local ADJ_FULLAUTOAIMINGTIME = 20 -- full auto only
+--local ADJ_FULLAUTOAIMINGTIME = 20 -- full auto only
 
 local ADJ_AUTOSWINGTIME = -0.3 -- for automatics
 local ADJ_AUTORECOILDELAY = -4 -- for automatics
@@ -23,8 +23,6 @@ local MOD_WEIGHTAIMINGTIME = 0.5 -- aiming time + (weight * mod)
 local MOD_WEIGHTRECOILDELAY = 0.5 -- recoil / (weight * mod)
 local MOD_WEIGHTSWINGTIME = 0.3 -- full auto swing + (weight * mod)
 
-local DELTA_BARREL_DAMAGE = 0.6 -- barrel length damage delta
-local DELTA_BARREL_SOUND = 6 --???
 
 --[[
 TODO: barrel optimization length calculations, and other stuff to consider
@@ -41,7 +39,7 @@ be factored in (yet).
     above optimal it starts to slow down due to friction.
 3) Length of barrels has a effect on noise. A longer barrel is quieter then a shorter one, as less gas escapes.
 4) Barrel lengths effect on accuracy is a mixed bag when it comes to long range. While longer barrels are generally
-    more accurate when the barrel is resting, it also means the bullet has longer 'barrel time' which means more 
+    more accurate when the barrel is resting, it also means the bullet has longer 'barrel time' which means more
     chance to waiver off target. Above/below optimal length and velocity causes additional bullet drop. Luckly long
     range shooting isn't really a thing in PZ, so this isn't much of a problem.
 5) Below optimal length, the extra gas escaping causes additional recoil.
@@ -51,20 +49,20 @@ be factored in (yet).
     and the center of weight changes while cycling.
 7) The effect on velocity from a barrel above/below is a definite curve. The closer we are to optimal length the less
     the effect is.
-    
+
 
 fps-fps*((((o-b)/o)**3)**2) this seems pretty damn close to matching, not sure were going to get much closer.
-looks like 'o' needs to be 80 for rifles, 30 for pistols to find a close match
+looks like 'o' needs to be 80 for rifles, 30 for pistols, 60 for shotguns to find a close match
 ]]
 
-local calcBarrelModifier = function(optimal, barrel, delta)
-    --return ((optimal-barrel)/optimal*(optimal-barrel)/optimal)*delta
+local calcBarrelModifier = function(optimal, barrel)
     return ((((optimal-barrel)/optimal)^3)^2)
 end
+
 local adjustDmgByBarrel = function(item, ammoType, damage)
     local barrel = ORGM.getBarrelLength(item)
     local optimal = ORGM.getOptimalBarrelLength(ammoType)
-    damage = damage - damage * calcBarrelModifier(optimal, barrel, 0.6)
+    return damage - damage * calcBarrelModifier(optimal, barrel)
 end
 
 ORGM.getBarrelLength = function(item)
@@ -84,23 +82,23 @@ end
 
 
 ORGM.getAbsoluteFirearmStats = function(instance, ammoData)
-    return { 
-        Weight = instance:getWeight(), 
+    return {
+        Weight = instance:getWeight(),
         ActualWeight = instance:getActualWeight(),
         MinDamage = (ammoData.MinDamage or instance:getMinDamage()) * ORGM.Settings.DamageMultiplier *(ORGM.NVAL/ORGM.PVAL/ORGM.NVAL),
         MaxDamage = (ammoData.MaxDamage or instance:getMaxDamage()) * ORGM.Settings.DamageMultiplier *(ORGM.NVAL/ORGM.PVAL/ORGM.NVAL),
         DoorDamage = ammoData.DoorDamage or instance:getDoorDamage(),
-        CriticalChance = Settings.DefaultCriticalChance, --ABS_CRITICALCHANCE,
-        AimingPerkCritModifier = Settings.DefaultAimingCritMod, -- ABS_AIMINGPERKCRITMOD, -- this is modifier * (level/2)
+        CriticalChance = Settings.DefaultCriticalChance,
+        AimingPerkCritModifier = Settings.DefaultAimingCritMod, -- this is modifier * (level/2)
         MaxHitCount = ammoData.MaxHitCount or instance:getMaxHitCount(),
         HitChance = instance:getHitChance(), -- redundant, we set to absolute
-        
+
         MinAngle = instance:getMinAngle(),
         MinRange = instance:getMinRangeRanged(),
         AimingTime = instance:getAimingTime(), -- redundant, we set to absolute
         RecoilDelay = instance:getRecoilDelay(), -- redundant, we set to absolute
         ReloadTime = instance:getReloadTime(),
-        MaxRange = instance:getMaxRange(),
+        MaxRange = ammoData.Range or instance:getMaxRange(),
         SwingTime = instance:getSwingTime(),
         AimingPerkHitChanceModifier = Settings.DefaultAimingHitMod --ABS_AIMINGPERKHITMOD,
     }
@@ -126,7 +124,7 @@ ORGM.adjustFirearmStatsByCategory = function(category, statsTable, effectiveWgt)
 end
 
 ORGM.adjustFirearmStatsByComponents = function(compnentTable, statsTable)
-    for _, mod in pairs(compnentTable) do    
+    for _, mod in pairs(compnentTable) do
       --statsTable.MaxRange = statsTable.MaxRange + mod:getMaxRange()
       --item:setMinRangeRanged(getMinRangeRanged() + mod:getMinRangeRanged())
       --setClipSize(getClipSize() + part.getClipSize());
@@ -137,22 +135,22 @@ ORGM.adjustFirearmStatsByComponents = function(compnentTable, statsTable)
       --statsTable.MinAngle = statsTable.MinAngle + mod:getAngle()
       --statsTable.MinDamage = statsTable.MinDamage + mod:getDamage()
       --statsTable.MaxDamage = statsTable.MaxDamage + mod:getDamage()
-      
+
       local cdetails = ORGM.getComponentData(mod) or { }
       statsTable.CriticalChance = statsTable.CriticalChance + (cdetails.CriticalChance or 0)
       statsTable.HitChance = statsTable.HitChance + (cdetails.HitChance or 0)
-      
+
       statsTable.SwingTime = statsTable.SwingTime + (cdetails.SwingTime or 0)
       statsTable.AimingTime = statsTable.AimingTime + (cdetails.AimingTime or 0)
       statsTable.ReloadTime = statsTable.ReloadTime + (cdetails.ReloadTime or 0)
       statsTable.RecoilDelay = statsTable.RecoilDelay + (cdetails.RecoilDelay or 0)
-      
+
       statsTable.MinDamage = statsTable.MinDamage + (cdetails.MinDamage or 0)
       statsTable.MaxDamage = statsTable.MaxDamage + (cdetails.MaxDamage or 0)
       statsTable.MinAngle = statsTable.MinAngle + (cdetails.MinAngle or 0)
       statsTable.MinRange = statsTable.MinRange + (cdetails.MinRange or 0)
       statsTable.MaxRange = statsTable.MaxRange + (cdetails.MaxRange or 0)
-      
+
     end
 end
 ORGM[10] = "86\070704944"
@@ -165,9 +163,20 @@ ORGM.adjustFirearmStatsByActionType = function(actionType, statsTable)
     end
 end
 
-ORGM.adjustFirearmStatsByAmmoType = function(ammoData, statsTable, effectiveWgt)
+ORGM.adjustFirearmStatsByAmmoType = function(weapon, ammoData, statsTable, effectiveWgt)
     -- adjust recoil relative to ammo, weight, barrel
-    statsTable.RecoilDelay = (ammoData.Recoil or 10) / (effectiveWgt * MOD_WEIGHTRECOILDELAY)
+    local length = ORGM.getBarrelLength(weapon) or 10 -- set to a default for safety
+    local optimal = data.OptimalBarrel or 30
+    local lenModifier = ORGM.calcBarrelModifier(optimal, length)
+    local recoil = ammoData.Recoil or 10
+    statsTable.RecoilDelay = (recoil+recoil*lenModifier) / (effectiveWgt * MOD_WEIGHTRECOILDELAY)
+    statsTable.MinDamage = statsTable.MinDamage - statsTable.MinDamage * lenModifier
+    statsTable.MaxDamage = statsTable.MaxDamage - statsTable.MaxDamage * lenModifier
+    statsTable.DoorDamage = statsTable.DoorDamage - statsTable.DoorDamage * lenModifier
+    statsTable.MaxRange = statsTable.MaxRange - statsTable.MaxRange * lenModifier
+
+    -- now for the noise...
+    --statsTable.SoundRadius = ???
 end
 
 ORGM.adjustFirearmStatsByFireMode = function(fireMode, alwaysFullAuto, statsTable)
@@ -184,7 +193,6 @@ ORGM.adjustFirearmStatsByFireMode = function(fireMode, alwaysFullAuto, statsTabl
         -- set swing time to a min value, or some fire too fast
         if statsTable.SwingTime < LIMIT_SWINGTIME then statsTable.SwingTime = LIMIT_SWINGTIME end
     end
-
 end
 
 ORGM.setWeaponStats = function(weapon, ammoType)
@@ -197,33 +205,33 @@ ORGM.setWeaponStats = function(weapon, ammoType)
     -- set inital values from defaults
     local stats = ORGM.getAbsoluteFirearmStats(details.instance, ammoData)
     -- adjust weight first
-    for _, mod in pairs(upgrades) do 
+    for _, mod in pairs(upgrades) do
       stats.ActualWeight = stats.ActualWeight + mod:getWeightModifier()
       stats.Weight = stats.Weight + mod:getWeightModifier()
     end
     -- effectiveWgt is the weight we use to calculate stats
     -- slings should not effect things like recoil or other stats
     local effectiveWgt = stats.ActualWeight - ((upgrades.Sling and upgrades.Sling:getWeightModifier()) or 0)
-    
+
     ----------------------------------------------------
     -- absolute values
     -- adjust swingtime based on weight
     stats.SwingTime = ABS_FULLAUTOSWINGTIME + (effectiveWgt * MOD_WEIGHTSWINGTIME) -- needs to also be adjusted by trigger
 
     ORGM.adjustFirearmStatsByCategory(details.category, stats, effectiveWgt)
-    ORGM.adjustFirearmStatsByAmmoType(ammoData, stats, effectiveWgt)
-    
+    ORGM.adjustFirearmStatsByAmmoType(weapon, ammoData, stats, effectiveWgt)
+
     ----------------------------------------------------
     -- adjust all by components first
     ORGM.adjustFirearmStatsByComponents(upgrades, stats)
     ORGM.adjustFirearmStatsByActionType(modData.actionType, stats)
-    
+
     -- set other relative ammoData adjustments
     stats.HitChance = stats.HitChance + (ammoData.HitChance or 0) - math.ceil(ORGM.PVAL-ORGM.NVAL)
     stats.CriticalChance = stats.CriticalChance - math.ceil(ORGM.PVAL-ORGM.NVAL)
-    
+
     ORGM.adjustFirearmStatsByFireMode(modData.selectFire, details.alwaysFullAuto, stats)
-    
+
     -- finalize any limits
     if stats.SwingTime < ABS_FULLAUTOSWINGTIME then stats.SwingTime = ABS_FULLAUTOSWINGTIME end
     stats.MinimumSwingTime = stats.SwingTime - 0.1
@@ -231,7 +239,7 @@ ORGM.setWeaponStats = function(weapon, ammoType)
     if stats.AimingTime < 1 then stats.AimingTime = 1 end
     stats.AimingTime = math.floor(stats.AimingTime) -- make sure to pass int
 
-    if stats.MinRange then 
+    if stats.MinRange then
         stats.MinRangeRanged = stats.MinRange -- change to proper name
         stats.MinRange = nil -- nil it so it doesnt nerf our melee range
     end
