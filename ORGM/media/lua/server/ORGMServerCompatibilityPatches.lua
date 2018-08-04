e6d4332e5ad151c85cb93e7a3a44dea98ce75f23
FWolfe <noreply@tekagis.ca>
2018-08-04 08:19:53 -0700

    Renamed files

--- ORGM/media/lua/server/ORGMServerCompatibilityPatches.lua
+++
@@ -1,5 +0,0 @@
-
-ORGM.Server.loadCompatibilityPatches = function()
-    -- nothing to do here
-    ORGM.log(ORGM.INFO, "All server compatibility patches injected")
-end
--- ORGM/media/lua/shared/ORGMSharedEventHooks.lua
+++
@@ -1,7 +0,0 @@
-
-Events.OnGameBoot.Add(ORGM.onBootBackwardsCompatibility) -- To be removed at a later date.
-Events.OnGameBoot.Add(ORGM.validateSettings)
-Events.OnGameBoot.Add(ORGM.limitFirearmYear)
-Events.OnGameBoot.Add(ORGM.loadCompatibilityPatches)
-
-Events.OnLoadSoundBanks.Add(ORGM.onLoadSoundBanks)
