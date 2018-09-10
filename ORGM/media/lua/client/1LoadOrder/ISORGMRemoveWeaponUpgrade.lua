bad4a12fe2395c1c4e9acc0b50d8c67c32918941
FWolfe <noreply@tekagis.ca>
2018-09-09 17:12:52 -0700

    set stats on attachment/removal

--- ORGM/media/lua/client/1LoadOrder/ISORGMRemoveWeaponUpgrade.lua
+++ ORGM/media/lua/client/1LoadOrder/ISORGMRemoveWeaponUpgrade.lua
@@ -7,20 +7,26 @@
 
 ]]
 
+local ORGM = ORGM
+local Firearm = ORGM.Firearm
+local Component = ORGM.Component
+
 --[[- Called when removing components.
 
 ]]
 function ISRemoveWeaponUpgrade:perform()
-    --ORGM.log(ORGM.DEBUG, "Removing "..self.part:getType())
     ORGM.log(ORGM.DEBUG, "Removing "..self.part:getType() .. ", weight="..tostring(self.part:getWeight())..'/'..tostring(self.part:getActualWeight())..'/'..tostring(self.part:getWeightModifier()))
 
-    ORGM.log(ORGM.DEBUG, "Weapon weight before "..tostring(self.weapon:getWeight()) .."/" .. tostring(self.weapon:getActualWeight()))
+    ORGM.log(ORGM.VERBOSE, "Weapon weight before "..tostring(self.weapon:getWeight()) .."/" .. tostring(self.weapon:getActualWeight()))
     self.weapon:detachWeaponPart(self.part)
-    ORGM.log(ORGM.DEBUG, "Weapon weight after "..tostring(self.weapon:getWeight()) .."/" .. tostring(self.weapon:getActualWeight()))
+    ORGM.log(ORGM.VERBOSE, "Weapon weight after "..tostring(self.weapon:getWeight()) .."/" .. tostring(self.weapon:getActualWeight()))
 
     -- delete the original part, give a fresh copy
-    local new = ORGM.Component.copy(self.part)
+    local new = Component.copy(self.part)
     self.character:getInventory():AddItem(new)
+    if Firearm.isFirearm(self.weapon) then
+        Firearm.Stats.set(self.weapon)
+    end
     -- needed to remove from queue / start next.
     ISBaseTimedAction.perform(self)
 end
--- ORGM/media/lua/client/1LoadOrder/ISORGMUpgradeWeapon.lua
+++ ORGM/media/lua/client/1LoadOrder/ISORGMUpgradeWeapon.lua
@@ -6,18 +6,19 @@
     @release 3.09
 
 ]]
-
+local ORGM = ORGM
+local Firearm = ORGM.Firearm
+local Component = ORGM.Component
 --[[- Called when attaching components.
 
 ]]
 function ISUpgradeWeapon:perform()
-    local def = ORGM.Component.getData(self.part)
+    local def = Component.getData(self.part)
     local data = self.part:getModData()
     if def and def.lastChanged and (data.BUILD_ID == nil or data.BUILD_ID < def.lastChanged) then
         -- handle orgm component update...
         ORGM.log(ORGM.INFO, "Obsolete component detected (" .. self.part:getType() .."). Running update function.")
-        --self.character:Say("Weapon Modification changed due to ORGM updates, resetting to default. Try attaching again.")
-        local new = ORGM.Component.copy(self.part)
+        local new = Component.copy(self.part)
         self.character:getInventory():Remove(self.part)
         self.character:getInventory():AddItem(new)
         self.part = new
@@ -31,12 +32,15 @@ function ISUpgradeWeapon:perform()
 
     ORGM.log(ORGM.DEBUG, "Installing "..self.part:getType() .. ", weight="..tostring(self.part:getWeight())..'/'..tostring(self.part:getActualWeight())..'/'..tostring(self.part:getWeightModifier()))
 
-    ORGM.log(ORGM.DEBUG, "Weapon weight before "..tostring(self.weapon:getWeight()) .."/" .. tostring(self.weapon:getActualWeight()))
+    ORGM.log(ORGM.VERBOSE, "Weapon weight before "..tostring(self.weapon:getWeight()) .."/" .. tostring(self.weapon:getActualWeight()))
 
     self.weapon:attachWeaponPart(self.part)
-    ORGM.log(ORGM.DEBUG, "Weapon weight after "..tostring(self.weapon:getWeight()) .."/" .. tostring(self.weapon:getActualWeight()))
+    ORGM.log(ORGM.VERBOSE, "Weapon weight after "..tostring(self.weapon:getWeight()) .."/" .. tostring(self.weapon:getActualWeight()))
     self.character:getInventory():Remove(self.part)
     self.character:setSecondaryHandItem(nil)
+    if Firearm.isFirearm(self.weapon) then
+        Firearm.Stats.set(self.weapon)
+    end
     -- needed to remove from queue / start next.
     ISBaseTimedAction.perform(self)
 end
