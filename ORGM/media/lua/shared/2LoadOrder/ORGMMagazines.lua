d855c8c4d2750645be910a64c5031bb3a419b7fe
FWolfe <noreply@tekagis.ca>
2019-04-23 13:41:24 -0700

    cleanup and variable name correction

--- ORGM/media/lua/shared/2LoadOrder/ORGMMagazines.lua
+++ ORGM/media/lua/shared/2LoadOrder/ORGMMagazines.lua
@@ -173,7 +173,7 @@ function MagazineGroup:find(ammoType, containerItem)
                 break
             end
 
-            if ammoType ~= 'any' and ammoType ~= modData.loadedAmmo then
+            if ammoType ~= 'any' and ammoType ~= modData.loadedAmmoType then
                 break
             end
             bestMagazine = currentItem
@@ -263,8 +263,8 @@ function MagazineType:setup(item)
     modData.currentCapacity = 0
     modData.clipType = self.clipType
     modData.magazineData = { }
-    modData.preferredAmmoType = 'any'
-    modData.loadedAmmo = nil
+    modData.strictAmmoType = nil
+    modData.loadedAmmoType = nil
     modData.BUILD_ID = ORGM.BUILD_ID
 end
 
@@ -295,7 +295,7 @@ Magazine.refill = function(item, count, ammoType)
         data.magazineData[i] = ammoType
     end
     data.currentCapacity = count
-    data.loadedAmmo = ammoType
+    data.loadedAmmoType = ammoType
 end
 --[[- Gets the table of registered magazines.
 
@@ -347,52 +347,4 @@ but called without needing a player or reloadable object.
 ]]
 Magazine.setup = function(magData, magItem)
     magData:setup(magItem)
-
-end
-
-
---[[- Finds the best matching magazine in a container.
-
-Search is based on the given magazine name and preferred load
-(can be specific round name, nil/any, or mixed), and the currentCapacity.
-
-This is called when reloading some guns and all magazines.
-
-Note magType and ammoType should NOT have the "ORGM." prefix.
-
-@tparam string magType name of a magazine
-@tparam nil|string ammoType 'any', 'mixed' or a specific ammo name
-@tparam ItemContainer containerItem
-
-@treturn nil|InventoryItem
-
-]]
-Magazine.findIn = function(magType, ammoType, containerItem)
-    if magType == nil then return nil end
-    local magData = MagazineTable[magType]
-    if not magData then return nil end -- not a valid orgm mag
-    if containerItem == nil then return nil end -- forgot the container item!
-    if ammoType == nil then ammoType = 'any' end
-    local bestMagazine = nil
-    local mostAmmo = -1
-    -- TODO: this needs a extra loop here, for possible alternate magazines
-    local items = containerItem:getItemsFromType(magType)
-
-    for i = 0, items:size()-1 do repeat
-        local currentItem = items:get(i)
-        local modData = currentItem:getModData()
-        if modData.currentCapacity == nil then -- magazine needs to be setup
-            Magazine.setup(magData, currentItem)
-        end
-        if modData.currentCapacity <= mostAmmo then
-            break
-        end
-
-        if ammoType ~= 'any' and ammoType ~= modData.loadedAmmo then
-            break
-        end
-        bestMagazine = currentItem
-        mostAmmo = modData.currentCapacity
-    until true end
-    return bestMagazine
 end
