28649d5cc950e5357a98efcd54f4cfe7bb18cda6
FWolfe <noreply@tekagis.ca>
2019-05-11 13:39:08 -0700

    fix component flags

--- ORGM/media/lua/shared/2LoadOrder/ORGMComponents.lua
+++ ORGM/media/lua/shared/2LoadOrder/ORGMComponents.lua
@@ -26,11 +26,11 @@ local ZombRand = ZombRand
 local Flags = ORGM.Component.Flags
 Flags.FIXED = 1
 
-Flags.MATCHGRADE
-Flags.THREADED
-Flags.PORTED
-Flags.FOLDING
-Flags.EXTENDABLE
+Flags.MATCHGRADE = 2
+Flags.THREADED = 4
+Flags.PORTED = 8
+Flags.FOLDING = 16
+Flags.EXTENDABLE = 32
 
 
 
